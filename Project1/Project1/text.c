625f375e78b8622052a72a33259b96a1a0b71637
liyuening <1067961666@qq.com>
2023-04-10 03:27:11 +0800

    数组指针打印二维数组

--- Project1/Project1/text.c
+++ Project1/Project1/text.c
@@ -1,29 +1,50 @@
 #define  _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-//写一段代码，告诉我们当前机器的字节序是什么
-//int check_sys()
-//{
-//	int a = 1;
-//	char* p=(char *)&a;
-//	return *p;
-//	//返回1小端
-//	// 01 00 00 00
-//	//返回0大端
-//	//00 00 00 01
-//}
-
-int check_sys()
+void print1(int(*pa)[5], int x, int y)
 {
-	int a = 1;
-	return *(char*)&a;
+	int i, j;
+	for (i = 0;i < x;i++)
+	{
+		//*(pa + i)数组名arr
+		for (j = 0;j < y;j++)
+		{
+			//printf("%d ", (*(pa + i))[j]);
+			//printf("%d ", *(*(pa + i)+j));
+			printf("%d ", pa[i][j]);
+		}
+		printf("\n");
+	}
 }
 int main()
 {
-	int ret = check_sys();
-	if (ret = 1)
-		printf("小端");
-	else
-		printf("大端");
+	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
+	print1(arr, 3, 5);
 	return 0;
 }
+////写一段代码，告诉我们当前机器的字节序是什么
+////int check_sys()
+////{
+////	int a = 1;
+////	char* p=(char *)&a;
+////	return *p;
+////	//返回1小端
+////	// 01 00 00 00
+////	//返回0大端
+////	//00 00 00 01
+////}
+//
+//int check_sys()
+//{
+//	int a = 1;
+//	return *(char*)&a;
+//}
+//int main()
+//{
+//	int ret = check_sys();
+//	if (ret = 1)
+//		printf("小端");
+//	else
+//		printf("大端");
+//	return 0;
+//}
